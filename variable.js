8917b0f2268424831171957a92b306212762665f
TraceOfLight <rickyjun45@gmail.com>
2022-02-01 06:06:29 +0900

    Async, Defer, Use Strict
    learning

---
+++ variable.js
@@ -0,0 +1,14 @@
+/* hello world */
+// Javascript is very flexible
+// flexible === dangerous
+// added ECMAScript 5
+'use strict';
+console.log = ('hello world!');
+
+let name = 'traceoflight';
+console.log('name');
+'name' = 'hello';
+console.log('name');
+let name = 'traceoflight';
+name = 'hello';
+console.log('name');
