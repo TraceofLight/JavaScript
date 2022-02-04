dedf5cbe7ee4d258a24cf9e13977380c35155c30
TraceOfLight <rickyjun45@gmail.com>
2022-02-05 03:43:31 +0900

    Arrow Function (2) + Class (1)
    functions and class, object

---
+++ class.js
@@ -0,0 +1,22 @@
+`Use strict`
+// Object-oriented programming
+// class: template
+// object: instance of a class
+// JavaScript classed
+//  - introduced in ES6
+//  - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+  // constructor
+  constructor(name, age) {
+    // fields
+    this.name = name;
+    this.age = age;
+  }
+
+  // methods
+  speak() {
+    console.log(`${this.name}: hello!`);
+  }
+}
--- function.js
+++ function.js
@@ -33,6 +33,7 @@ const ToL = {name: `ToL`};
 changeName(ToL);
 console.log(ToL);
 
+showMessage(`Hi!`, 'Mom'); // hoisted function declaration
 // 3. Default parameters (added in ES6)
 function showMessage(message, from) {
   if (from === undefined) {
@@ -40,4 +41,134 @@ function showMessage(message, from) {
   }
   console.log(`${message} by ${from}`);
 }
-showMessage('Hi!');
+showMessage(`Hi!`);
+
+// 4. Rest parameters (added in ES6)
+function printAll(...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+  }
+//  for (const arg of args) {
+//    console.log(arg);
+//  }
+
+// args.forEach((arg) => console.log(arg));
+}
+printAll(`dream`, `coding`, `TraceOfLight`);
+
+// 5. Local scope
+let globalMessage = `global`; // global variable
+function printMessage() {
+  let message = `hello`;
+  console.log(message); // local variable
+  console.log(globalMessage);
+  function printAnother() {
+    console.log(message);
+    let childMessage = `hello`;
+  }
+  // console.log(childMessage); // error
+  // return undefined; is hiddened
+}
+printMessage();
+
+// 6. Return a value
+function sum(a, b) {
+  return a + b;
+}
+const result = sum(1, 2); // 3
+console.log(`sum: ${sum(1, 2)}`);
+
+// 7. Early return, Early exit
+// bad
+function upgradeuser(user) {
+  if (user.point > 10) {
+    // long upgrade logic...
+  }
+}
+
+// good
+function upgradeuser(user) {
+  if (user.point <= 10) {
+    return;
+  }
+  // long upgrade logic...
+}
+
+// First-class function
+// function are treated like any other variable
+// can be assigned as a value to variable
+// can be passed as an argument to other functions
+// can be returned by another function
+
+// 1. Fuction expression
+// a function declaration can be called earlier than it is defined. (hoisted)
+// a function expression is created when the execution reaches it.
+
+const print = function () { // anonymous function
+  console.log(`print`);
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+
+// 2. Callback function using function expression
+function randomQuiz(answer, printYes, printNo) {
+  if (answer === `love you`) {
+    printYes();
+  }
+  else {
+    printNo();
+  }
+}
+// anonymous function
+const printYes = function () {
+  console.log(`yes!`);
+}
+// named function
+// better debugging in debugger's stack traces
+// recursions
+const printNo = function print() {
+  console.log(`no!`);
+};
+randomQuiz(`wrong`, printYes, printNo);
+randomQuiz(`love you`, printYes, printNo);
+
+// Arrow function
+// always anonymous
+// const simplePrint = function () {
+//   console.log(`simplePrint!`);
+// }
+
+const simplePrint = () => console.log(`simplePrint!`);
+const add = (a, b) => a + b;
+const simpleMultiply = (a, b) => {
+  // do something more
+  return a * b;
+}
+
+// IIFE: Immediately Invoked Function Expression
+(function hello() {
+  console.log(`IIFE`);
+})();
+
+// Fun quiz time
+// function calculate (command, a, b)
+// command: add, abstract, divide, multiply, remainder
+
+const calculate = function (command, a, b) {
+  switch (command) {
+    case 'add' :
+      console.log(a + b);
+    case 'abstract' :
+      console.log(a - b);
+    case 'divide' :
+      console.log(a / b);
+    case 'multiply' :
+      console.log(a * b);
+    case 'remainder' :
+      console.log(a % b);
+    default : console.log('wrong');
+  }
+}
