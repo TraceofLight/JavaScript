685e3271e071a24ccd5ecad99514771fcb6296c7
TraceOfLight <rickyjun45@gmail.com>
2022-02-05 23:57:21 +0900

    Class (2)
    Getter & setters, Fields,Static properties & methods

--- class.js
+++ class.js
@@ -20,3 +20,62 @@ class Person {
     console.log(`${this.name}: hello!`);
   }
 }
+
+const TraceofLight = new Person('TraceofLight', 20);
+console.log(TraceofLight.age);
+console.log(TraceofLight.name);
+console.log(typeof TraceofLight.name);
+TraceofLight.speak();
+
+// 2. Getter and setters
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get age() {
+    return this._age;
+
+  }
+
+  set age(value) {
+    // if (value < 0) {
+    //  throw Error('age can not be negative');
+    // }
+    this._age = value < 0 ? 0 : value;
+  }
+}
+
+const user1 = new User('Steve', 'Job', -1);
+console.log(user1.age);
+
+// 3. Fields (public, private)
+// Too soon!
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Classes/Public_class_fields
+class Experiment {
+  publicField = 2;
+  #privateField = 0;
+}
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+// 4. Static properties and methods
+// Too soon!
+class Article {
+  static publisher = 'Dream Coding';
+  constructor(articleNumber) {
+    this.articleNumber = articleNumber;
+  }
+
+  static printPublisher() {
+    console.log(Article.publisher);
+  }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(Article.publisher);
+Article.printPublisher();
